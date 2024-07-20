7c19c13b465990936c96410bcfbb659011938e2d
Hersh <sanganihersh@gmail.com>
2024-07-19 20:12:39 -0400

    Make a working etch a sketch

--- javascript.js
+++ javascript.js
@@ -3,12 +3,11 @@ sketchContainer = document.querySelector('.sketchContainer');
 
 // Put in a single row of evenly spaced flex boxes inside the sketchContainer
 
-let numBoxesPerRow = 3;
+let numBoxesPerRow = 40;
 //const boxArray = [];
 
 for (i = 0; i < numBoxesPerRow ** 2; i++) {
     const box = document.createElement('div');
-    box.textContent = `BOX`;
 
     // Style
     box.className = 'box';
@@ -16,7 +15,7 @@ for (i = 0; i < numBoxesPerRow ** 2; i++) {
     // Calculate desired width of box based on how many total boxes
     // 2 elements, 1 gap, 3 elements 2 gaps 
     // Split up 90% of the space
-    let boxWidthPercent = 90/numBoxesPerRow;
+    let boxWidthPercent = 90 / numBoxesPerRow;
     box.style.width = `${boxWidthPercent}%`
     // Height distribution matches the width
     let boxHeightPercent = boxWidthPercent;
@@ -27,13 +26,27 @@ for (i = 0; i < numBoxesPerRow ** 2; i++) {
 
     // add a listener to each box to change its background color on hover
 
-    box.addEventListener('mouseenter', ()=> mouseEnterEffect(box));
+    box.addEventListener('mouseenter', () => {
+        //console.log(box.style.opacity);
+        if (isMouseDown) {
+            box.style.opacity = Math.min(Number(box.style.opacity) + 0.2, 1);
+            //console.log(box.style.opacity);
+        }
+    });
+
+
+
 
 
 }
 
-function mouseEnterEffect(element){
-    //this.setAttribute("class","highlight");
-    currentOpacity = element.style.opacity;
-    console.log(element.style.opacity);
-}
+let isMouseDown = 0;
+// Listen for the mousedown event to set the flag to true
+document.addEventListener('mousedown', () => {
+    isMouseDown = true;
+});
+
+// Listen for the mouseup event to reset the flag to false
+document.addEventListener('mouseup', () => {
+    isMouseDown = false;
+});
